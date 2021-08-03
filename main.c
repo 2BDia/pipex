1c1141c1c372bacecb3089c1f4f52b37e7a4d369
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2021-08-03 15:40:25 +0200

    works with any input in the command brackets

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 13:05:57 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/08/03 14:18:14 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/08/03 15:39:52 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,8 @@ static char	*cut_cmd_flags(char **argv, char **cmd)
 	len = 0;
 	while (argv[2][i])
 		cmd[1][len++] = argv[2][i++];
-	cmd[1][i] = '\0';
+	cmd[1][len] = '\0';
+	i = 0;
 	if (ft_strlen(cmd[1]) == 0)
 	{
 		free(cmd[1]);
@@ -51,43 +52,57 @@ static char	*cut_cmd_flags(char **argv, char **cmd)
 	return (str);
 }
 
-static void	cut_flags_args(char **cmd)
+static int	cut_flags_args(char **cmd)
 {
 	int		i;
+	int		icpy;
 	int		len;
+	int		n;
 	char	*tmp;
 
 	i = 0;
-	len = 0;
-	while (cmd[1][len] && cmd[1][len] == '-')
+	n = 1;
+	tmp = ft_strdup(cmd[1]);
+	free(cmd[1]);
+	while (tmp[i])
 	{
-		while (cmd[1][len] && !is_space(cmd[1][len]))
-			len++;
-		while (is_space(cmd[1][len]))
-			len++;
-		if (cmd[1][len] == '-')
-			continue ;
-		if (!cmd[1][len])
-			return ;
-		else
+		len = 0;
+		if (!is_space(tmp[i]))
 		{
-			tmp = ft_strdup(cmd[1]);
-			free(cmd[1]);
-			cmd[1] = (char *)malloc(sizeof(char) * (len + 1));
-			while (i < len)
+			icpy = i;
+			while (!is_space(tmp[i]) && tmp[i])
 			{
-				cmd[1][i] = tmp[i];
+				len++;
 				i++;
 			}
-			cmd[1][i] = '\0';
-			free(tmp);
+			cmd[n] = (char *)malloc(sizeof(char) * (len + 1));
+			len = 0;
+			while (!is_space(tmp[icpy]) && tmp[icpy])
+				cmd[n][len++] = tmp[icpy++];
+			cmd[n][len] = '\0';
 		}
+		while (is_space(tmp[i]))
+			i++;
+		n++;
 	}
+	free(tmp);
+	return (n);
 }
 
-static int	count_args()
+static int	count_args(char *arg)
 {
-	
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (arg[i])
+	{
+		if ((is_space(arg[i + 1]) || arg[i + 1] == '\0') && !is_space(arg[i]))
+			count++;
+		i++;
+	}
+	return (count);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -96,20 +111,24 @@ int	main(int argc, char **argv, char **env)
 	char		**path;
 	char		*tmp;
 	int			i;
+	int			n;
 
 	i = 0;
 	if (argc != 5)
 		return (0);
-		
-	cmd = (char **)malloc(sizeof(char *) * 4);
+	cmd = (char **)malloc(sizeof(char *) * count_args(argv[2]) + 1);
 	cmd[0] = cut_cmd_flags(argv, cmd);
 	// if (!cmd[0])
 	// 	error free etc
-	cut_flags_args(cmd);
-	cmd[2] = NULL;
-	cmd[3] = NULL;
+	if (cmd[1])
+	{
+		n = cut_flags_args(cmd);
+		cmd[n] = NULL;
+	}
 	printf("cmd[0]=%s\n", cmd[0]);
 	printf("cmd[1]=%s\n", cmd[1]);
+	printf("cmd[2]=%s\n", cmd[2]);
+	printf("cmd[3]=%s\n", cmd[3]);
 	path = split_paths(env);
 	tmp = ft_strdup(cmd[0]);
 	if (fork() == 0) //Child
