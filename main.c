5b74f68ffa4f515615cbe7853b423c6573dd922b
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2021-08-04 13:45:56 +0200

    dup2 works for in file

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 13:05:57 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/08/03 16:35:17 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/08/04 13:44:07 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static char	*cut_cmd_flags(char **argv, char **cmd)
+static char	*cut_cmd_flags(char **argv, char **cmd, int mod)
 {
 	char	*str;
 	int		len;
@@ -21,28 +21,28 @@ static char	*cut_cmd_flags(char **argv, char **cmd)
 	len = 0;
 	i = 0;
 	
-	while (argv[2][len] && !is_space(argv[2][len]))
+	while (argv[mod][len] && !is_space(argv[mod][len]))
 		len++;
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	while (argv[2][i] && !is_space(argv[2][i]))
+	while (argv[mod][i] && !is_space(argv[mod][i]))
 	{
-		str[i] = argv[2][i];
+		str[i] = argv[mod][i];
 		i++;
 	}
 	str[i] = '\0';
 	i--;
-	while (is_space(argv[2][++i]))
+	while (is_space(argv[mod][++i]))
 		len++;
-	while (argv[2][len])
+	while (argv[mod][len])
 		len++;
 	cmd[1] = (char *)malloc(sizeof(char) * ((len - i) + 1));
 	if (!cmd[1])
 		return (NULL);
 	len = 0;
-	while (argv[2][i])
-		cmd[1][len++] = argv[2][i++];
+	while (argv[mod][i])
+		cmd[1][len++] = argv[mod][i++];
 	cmd[1][len] = '\0';
 	i = 0;
 	if (ft_strlen(cmd[1]) == 0)
@@ -108,19 +108,35 @@ static int	count_args(char *arg)
 	return (count);
 }
 
-int	main(int argc, char **argv, char **env)
+static void	child(char **cmd1, char **path, char **env, char **argv)
 {
-	char		**cmd;
-	char		**path;
-	char		*tmp;
-	int			i;
-	int			n;
+	char	*tmp;
+	int		i;
+	int		fd;
 
 	i = 0;
-	if (argc != 5)
-		return (0);
-	cmd = (char **)malloc(sizeof(char *) * (count_args(argv[2]) + 1));
-	cmd[0] = cut_cmd_flags(argv, cmd);
+	fd = open(argv[1], O_RDONLY);
+	if (dup2(fd, STDIN_FILENO) == 1)
+		write(1, "dup2 failed\n", 12); //close free etc
+	tmp = ft_strdup(cmd1[0]);
+	while (execve(*cmd1, cmd1, env) == -1)
+	{
+		free(cmd1[0]);
+		cmd1[0] = ft_strjoin(path[i], tmp);
+		i++;
+	}
+	close(fd);
+	close(0);
+	free(tmp);
+}
+
+static char	**parse_cmd(char **argv, int mod)
+{
+	char	**cmd;
+	int		n;
+
+	cmd = (char **)malloc(sizeof(char *) * (count_args(argv[mod]) + 1));
+	cmd[0] = cut_cmd_flags(argv, cmd, mod);
 	// if (!cmd[0])
 	// 	error free etc
 	if (cmd[1])
@@ -130,25 +146,32 @@ int	main(int argc, char **argv, char **env)
 		// 	error free etc
 		cmd[n] = NULL;
 	}
-	// printf("cmd[0]=%s\n", cmd[0]);
-	// printf("cmd[1]=%s\n", cmd[1]);
-	// printf("cmd[2]=%s\n", cmd[2]);
-	// printf("cmd[3]=%s\n", cmd[3]);
+	printf("cmd[0]=%s\n", cmd[0]);
+	printf("cmd[1]=%s\n", cmd[1]);
+	printf("cmd[2]=%s\n", cmd[2]);
+	printf("cmd[3]=%s\n", cmd[3]);
+	return (cmd);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	char		**cmd1;
+	char		**cmd2;
+	char		**path;
+
+	if (argc != 5)
+		return (0);
+	cmd1 = parse_cmd(argv, 2);
+	cmd2 = parse_cmd(argv, 3);
 	path = split_paths(env);
-	tmp = ft_strdup(cmd[0]);
 	if (fork() == 0) //Child
 	{
-		while (execve(*cmd, cmd, env) == -1)
-		{
-			free(cmd[0]);
-			cmd[0] = ft_strjoin(path[i], tmp);
-			i++;
-		}
+		child(cmd1, path, env, argv);
 	}
-	free(tmp);
-	free_arrays(cmd);
+	free_arrays(cmd1);
+	free_arrays(cmd2);
 	free_arrays(path);
-	// system("leaks pipex");
+	system("leaks pipex");
 	return (0);
 }
 
@@ -178,6 +201,3 @@ int	main(int argc, char **argv, char **env)
 	//cat(chemin + commande)
 //execve(char *commande, char **cmd, char **environnement)
 //char **cmd = {"grep", "-n", "q"}
-
-
-// /Users/rvan-aud/.brew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki
