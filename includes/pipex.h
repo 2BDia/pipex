e0da01b0edf78a00655a0c7cc90a256f55e02058
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2021-08-12 15:01:21 +0200

    cleaned pipex.h and removed extra files

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 13:06:17 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/08/12 14:47:23 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/08/12 14:59:10 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,38 @@
 # include <fcntl.h>
 # include "libft.h"
 
-# include <stdio.h> //to remove
+/*
+** cut_cmd.c
+*/
+char	*cut_cmd_flags(char **argv, char **cmd, int mod);
+int		cut_flags_args(char **cmd);
 
-// typedef struct s_params
-// {
-// 	char	*infile;
-// 	char	*outfile;
-// 	char	*cmd1;
-// 	char	*cmd2;
-// }	t_params;
-
+/*
+** free.c
+*/
 void	close_err_dup2(int fd, char **path, char ***cmd, int *pipefd);
 void	free_arrays(char **cmd);
 void	free_close(char **path, char ***cmd, int *pipefd, int mod);
 void	exit_msg(char **path, char ***cmd, int *pipefd, int mod);
 
-char	**split_paths(char **env);
-
-int		is_space(char c);
-
+/*
+** parse_cmd.c
+*/
 char	**parse_cmd(char **argv, int mod);
 
-char	*cut_cmd_flags(char **argv, char **cmd, int mod);
-int		cut_flags_args(char **cmd);
+/*
+** path_handling.c
+*/
+char	**split_paths(char **env);
 
+/*
+** pipex.c
+*/
 void	pipex(char ***cmd, char **env, int *pipefd);
+
+/*
+** utils.c
+*/
+int		is_space(char c);
 
 #endif
