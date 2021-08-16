a570b17b4021804a97fa71fc0fb7ac54e53c6ce4
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2021-08-16 17:25:35 +0200

    before trying to restructure pipex

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 13:05:57 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/08/16 15:25:43 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/08/16 17:20:51 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,9 @@ int	main(int argc, char **argv, char **env)
 		return (errors_main(cmd, 1));
 	if (pid == 0)
 		pipex(cmd, pipefd, vars);
-	write(1, "ok\n", 3);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	wait(0);
 	free_close(NULL, cmd, pipefd, 1);
 	// system("leaks pipex");
 	return (0);
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 14:44:22 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/08/16 16:19:51 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/08/16 17:24:30 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,34 @@ static void	exec_cmd(char **cmd, char **env, char **path)
 	}
 }
 
-static void	middle(char ***cmd, int *pipefd, char **path, t_vars vars, int *pipefd2)
-{
-	
-}
+// static void	middle(char ***cmd, int *pipefd, char **path, t_vars vars, int *pipefd_2)
+// {
+// 	if (dup2(pipefd[0], STDIN_FILENO) == -1)
+// 		write(2, "error dup2\n", 11);
+// 	if (dup2(pipefd_2[1], STDOUT_FILENO) == -1)
+// 		write(2, "error dup2\n", 11);
+// 	close(pipefd[0]);
+// 	close(pipefd[1]);
+// 	exec_cmd(cmd[1], vars.env, path);
+// }
+
+// static void	prep_middle(char ***cmd, int *pipefd, char **path, t_vars vars)
+// {
+// 	int	pipefd_2[2];
+// 	int	pid;
+// 	char	buffer[80];
+
+// 	if (pipe(pipefd_2) == -1)
+// 		write(2, "error pipe\n", 11);
+// 	pid = fork();
+// 	if (pid == -1)
+// 		write(2, "error fork\n", 11);
+// 	if (pid == 0)
+// 		middle(cmd, pipefd, path, vars, pipefd_2);
+// 	write(2, "ok2\n", 4);
+// 	read(pipefd_2[0], buffer, 80);
+// 	printf("buffer=%s\n", buffer);
+// }
 
 static void	first(char ***cmd, int *pipefd, char **path, t_vars vars)
 {
@@ -48,55 +72,43 @@ static void	first(char ***cmd, int *pipefd, char **path, t_vars vars)
 		close_err_dup2(fd, path, cmd, pipefd);
 	if (dup2(pipefd[1], STDOUT_FILENO) == -1)
 		close_err_dup2(fd, path, cmd, pipefd);
-	write(2, "ok1\n", 4);
 	close(fd);
+	close(pipefd[0]);
 	exec_cmd(cmd[0], vars.env, path);
 	exit_msg(path, cmd, pipefd, 4);
 }
 
 void	pipex(char ***cmd, int *pipefd, t_vars vars)
 {
-	int		pid;
+	int		pid1;
+	int		pid2;
 	int		fd;
 	char	**path;
 	// int		i;
-	// char	buffer[80];
-	int		pipefd2[2];
 
 	path = split_paths(vars.env);
-	pid = fork();
-	if (pid == -1)
+	pid1 = fork();
+	if (pid1 == -1)
 		exit_msg(path, cmd, pipefd, 0);
-	if (pid == 0)
+	if (pid1 == 0)
 		first(cmd, pipefd, path, vars);
 	wait(0);
-	write(1, "ok2\n", 4);
-	pipe(pipefd2);
-	pid = fork();
-	if (pid == -1)
-		exit_msg(path, cmd, pipefd, 0);
-	if (pid == 0)
-		middle(cmd, pipefd, path, vars, pipefd2);
-	free_close(path, cmd, pipefd, 0);
-	write(2, "ok3\n", 4);
-	// read(pipefd2[0], buffer, 80);
-	// printf("buffer=%s\n", buffer);
+	// write(1, "ok1\n", 4);
+	// prep_middle(cmd, pipefd, path, vars);
 	// i = 0;
 	// while (i < vars.n - 3)
 	// {
-	// 	vars.i = i + 1;
 	// 	pid = fork();
 	// 	if (pid == -1)
 	// 		exit_msg(path, cmd, pipefd, 0);
 	// 	if (pid == 0)
 	// 		middle(cmd, pipefd, path, vars);
-	// 	wait(0);
 	// 	i++;
 	// }
 	fd = open(*cmd[vars.n - 1], O_RDWR | O_CREAT | O_TRUNC, 0666);
 	if (fd == -1)
 		exit_msg(path, cmd, pipefd, 1);
-	if (dup2(pipefd2[0], STDIN_FILENO) == -1)
+	if (dup2(pipefd[0], STDIN_FILENO) == -1)
 		close_err_dup2(fd, path, cmd, pipefd);
 	if (dup2(fd, STDOUT_FILENO) == -1)
 		close_err_dup2(fd, path, cmd, pipefd);
