4b9f1666734fd6ddeda3674835c70b6da2d14a6b
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2021-08-17 14:58:48 +0200

    yes

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 14:44:22 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/08/17 14:22:51 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/08/17 14:56:52 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,50 +32,21 @@ static void	exec_cmd(char **cmd, char **env, char **path)
 	}
 }
 
-// static void	middle(char ***cmd, int *pipefd, char **path, t_vars vars, int *pipefd_2)
-// {
-// 	if (dup2(pipefd[0], STDIN_FILENO) == -1)
-// 		write(2, "error dup2\n", 11);
-// 	if (dup2(pipefd_2[1], STDOUT_FILENO) == -1)
-// 		write(2, "error dup2\n", 11);
-// 	close(pipefd[0]);
-// 	close(pipefd[1]);
-// 	exec_cmd(cmd[1], vars.env, path);
-// }
-
-// static void	prep_middle(char ***cmd, int *pipefd, char **path, t_vars vars)
-// {
-// 	int	pipefd_2[2];
-// 	int	pid;
-// 	char	buffer[80];
-
-// 	if (pipe(pipefd_2) == -1)
-// 		write(2, "error pipe\n", 11);
-// 	pid = fork();
-// 	if (pid == -1)
-// 		write(2, "error fork\n", 11);
-// 	if (pid == 0)
-// 		middle(cmd, pipefd, path, vars, pipefd_2);
-// 	write(2, "ok2\n", 4);
-// 	read(pipefd_2[0], buffer, 80);
-// 	printf("buffer=%s\n", buffer);
-// }
-
-static void	first(char ***cmd, int *pipefd, char **path, t_vars vars)
+static void	first(char ***cmd, int *pipe1, char **path, t_vars vars)
 {
 	int	fd;
 
 	fd = open(*cmd[vars.n - 2], O_RDONLY);
 	if (fd == -1)
-		exit_msg(path, cmd, pipefd, 2);
+		exit_msg(path, cmd, pipe1, 2);
 	if (dup2(fd, STDIN_FILENO) == -1)
-		close_err_dup2(fd, path, cmd, pipefd);
-	if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-		close_err_dup2(fd, path, cmd, pipefd);
+		close_err_dup2(fd, path, cmd, pipe1);
+	if (dup2(pipe1[1], STDOUT_FILENO) == -1)
+		close_err_dup2(fd, path, cmd, pipe1);
 	close(fd);
-	close(pipefd[0]);
+	close(pipe1[0]);
 	exec_cmd(cmd[0], vars.env, path);
-	exit_msg(path, cmd, pipefd, 4);
+	exit_msg(path, cmd, pipe1, 4);
 }
 
 static void	last(char ***cmd, int *pipe2, char **path, t_vars vars)
@@ -96,19 +67,19 @@ static void	last(char ***cmd, int *pipe2, char **path, t_vars vars)
 	exit_msg(path, cmd, pipe2, 4);
 }
 
-static void	middle_fork(char ***cmd, char **path, t_vars vars, int *pipe1, int *pipe2)
+static void	middle_fork(char ***cmd, char **path, t_vars vars, int *pipein, int *pipeout, int i)
 {
-	if (dup2(pipe1[0], STDIN_FILENO) == -1)
-		close_err_dup2(-1, path, cmd, pipe2);
-	if (dup2(pipe2[1], STDOUT_FILENO) == -1)
-		close_err_dup2(-1, path, cmd, pipe2);
-	close(pipe1[1]);
-	close(pipe2[0]);
-	exec_cmd(cmd[1], vars.env, path);
-	exit_msg(path, cmd, pipe2, 4);
+	if (dup2(pipein[0], STDIN_FILENO) == -1)
+		close_err_dup2(-1, path, cmd, pipeout);
+	if (dup2(pipeout[1], STDOUT_FILENO) == -1)
+		close_err_dup2(-1, path, cmd, pipeout);
+	close(pipein[1]);
+	close(pipeout[0]);
+	exec_cmd(cmd[i], vars.env, path);
+	exit_msg(path, cmd, pipeout, 4);
 }
 
-static int	middle(char ***cmd, t_vars vars, int *pipe1, char **path, int *pipe2)
+static int	middle(char ***cmd, t_vars vars, int *pipein, char **path, int *pipeout, int i)
 {
 	int	pid;
 
@@ -116,11 +87,10 @@ static int	middle(char ***cmd, t_vars vars, int *pipe1, char **path, int *pipe2)
 	if (pid == -1)
 		return (errors_main(cmd, 1));
 	if (pid == 0)
-		middle_fork(cmd, path, vars, pipe1, pipe2);
-	close(pipe1[0]);
-	close(pipe1[1]);
-	close(pipe2[1]);
-	write(2, "ok\n", 3);
+		middle_fork(cmd, path, vars, pipein, pipeout, i);
+	close(pipein[0]);
+	close(pipein[1]);
+	close(pipeout[1]);
 	return (1);
 }
 
@@ -131,6 +101,8 @@ int	pipex(char ***cmd, t_vars vars)
 	int		pipe1[2];
 	int		pipe2[2];
 	char	**path;
+	int		i;
+	int		check;
 
 	path = split_paths(vars.env);
 	if (pipe(pipe1) == -1)
@@ -140,18 +112,52 @@ int	pipex(char ***cmd, t_vars vars)
 		return (errors_main(cmd, 1));
 	if (pid1 == 0)
 		first(cmd, pipe1, path, vars);
-	if (pipe(pipe2) == -1)
-		return (errors_main(cmd, 0));
-	if (!middle(cmd, vars, pipe1, path, pipe2))
-		return (0);
-	write(2, "ok\n", 3);
+	i = 0;
+	check = 0;
+	while (i < vars.n - 4)
+	{
+		i += 1;
+		printf("i=%d\n", i);
+		if (check == 0)
+		{
+			if (pipe(pipe2) == -1)
+				return (errors_main(cmd, 0));
+		}
+		else
+		{
+			if (pipe(pipe1) == -1)
+				return (errors_main(cmd, 0));
+		}
+		if (check == 0)
+		{
+			if (!middle(cmd, vars, pipe1, path, pipe2, i))
+				return (0);
+		}
+		else
+		{
+			if (!middle(cmd, vars, pipe2, path, pipe1, i))
+				return (0);
+		}
+		if (check == 0)
+			check = 1;
+		else
+			check = 0;
+		wait(0);
+	}
 	pid2 = fork();
 	if (pid2 == -1)
 		return (errors_main(cmd, 1));
-	if (pid2 == 0)
-		last(cmd, pipe2, path, vars);
+	if (check == 1)
+	{
+		if (pid2 == 0)
+			last(cmd, pipe2, path, vars);
+	}
+	else
+	{
+		if (pid2 == 0)
+			last(cmd, pipe1, path, vars);
+	}
 	free_close(path, cmd, pipe1, 1);
-	wait(0);
 	wait(0);
 	wait(0);
 	return (0);
